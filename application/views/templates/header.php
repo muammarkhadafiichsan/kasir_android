50c2709d73ef62515097188d24feaf15e09deee3
fabiputra <fabiputra55@gmail.com>
2019-12-24 21:53:41 +0700

    hhh

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,9 +9,10 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Dashboard</title>
+  <title>Admin</title>
 
   <!-- Custom fonts for this template-->
+  <link rel="shortcut icon" type="text/css" href="<?php echo base_url() . "assets/img/logo_bundar.png"; ?>" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
   <link href="<?= base_url('assets/')?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   
